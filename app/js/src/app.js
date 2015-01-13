4c461ad60c2fccec0ff69d6c4fbbb97e51214e37
Jean-Louis GUENEGO <jlguenego@gmail.com>
2015-01-13 10:58:21 +0100

    polyfill

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -5,6 +5,30 @@ if (typeof String.prototype.startsWith != 'function') {
 	};
 }
 
+//polyfill for Array.prototype.find
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
 (function() {
 	var app = angular.module('angular_cours_app',
 		[ 'ngRoute', 'myLayout', 'myServices', 'angularMarkdownInclude']);
